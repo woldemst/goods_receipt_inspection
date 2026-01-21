6ab131a7cb42f2eef2a052e1d1e8f9163fc43704
Waldemar <72108511+woldemst@users.noreply.github.com>
2026-01-21 14:04:47 +0100

    added logining

--- client/App.tsx
+++ client/App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import LoginScreen from "./src/screens/LoginScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as SecureStore from "expo-secure-store";
+import React, { useEffect, useState } from "react";
 
+import LoginScreen from "./src/screens/LoginScreen";
+import ProjectListScreen from "./src/screens/ProjectListScreen";
+import { api } from "./src/api";
+
+const Stack = createNativeStackNavigator;
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <LoginScreen />
-    </View>
-  );
+	const [token, setToken] = useState<string | null>(null);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		async function init() {
+			const savedToken = await SecureStore.getItemAsync("token");
+			if (savedToken) {
+				setToken(savedToken);
+				api.defaults.headers.common.Authorization = `Bearer ${savedToken}`;
+			}
+		}
+		init();
+	}, []);
+	return (
+		<NavigationContainer>
+			<Stack.Navigator screenOptions={{ headerShown: false }}>
+
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
